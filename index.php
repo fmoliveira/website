70b86de3a20357379c0549b80e7e45bafb11d77e
Filipe Oliveira <contato@fmoliveira.com.br>
2014-03-23 00:41:50 -0300

    Iniciando meu novo website pessoal.

---
+++ index.php
@@ -0,0 +1,11 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Filipe Oliveira</title>
+    <link rel="stylesheet" href="css/normalize.css"/>
+    <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
