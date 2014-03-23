b50aac882103cbd9887bb5ab098f610897918704
Filipe Oliveira <contato@fmoliveira.com.br>
2014-03-23 09:03:21 -0300

    Layout do menu principal começando a ficar definido.

--- index.php
+++ index.php
@@ -1,11 +1,62 @@
 ﻿<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+    <!-- Meta tags -->
     <title>Filipe Oliveira</title>
+
+    <!-- Folhas de estilo -->
     <link rel="stylesheet" href="css/normalize.css"/>
+    <link rel="stylesheet" href="css/fmoliveira.css"/>
+
+    <!-- Scripts -->
     <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
 </head>
 <body>
+
+    <section id="wrapper">
+
+        <div id="menu">
+            <nav>
+                <ul id="perfil">
+                    <li><img src="imagens/fmoliveira.png" alt="Filipe Oliveira" /></li>
+                    <li><strong>Filipe Oliveira</strong></li>
+                    <li>Engenheiro de Computação</li>
+                </ul>
+            </nav>
+
+            <nav>
+                <ul id="social">
+                    <li><a href="#" class="twitter"><img src="imagens/twitter.png" alt="Twitter" /></a></li>
+                    <li><a href="#" class="linkedin"><img src="imagens/linkedin.png" alt="Twitter" /></a></li>
+                    <li><a href="#" class="delicious"><img src="imagens/delicious.png" alt="Twitter" /></a></li>
+                    <li><a href="#" class="soundcloud"><img src="imagens/soundcloud.png" alt="Twitter" /></a></li>
+                </ul>
+                <div class="clear"></div>
+            </nav>
+
+            <nav>
+                <ul id="links" class="menu-vertical">
+                    <li><a href="#"><img src="imagens/blog.png" alt="Blog" /> Blog</a></li>
+                    <li><a href="#"><img src="imagens/projetos.png" alt="Projetos" /> Projetos</a></li>
+                    <li><a href="#"><img src="imagens/paixoes.png" alt="Paixões" /> Paixões</a></li>
+                    <li><a href="#"><img src="imagens/curriculo.png" alt="Currículo" /> Currículo</a></li>
+                    <li><a href="#"><img src="imagens/sobre-mim.png" alt="Sobre mim" /> Sobre mim</a></li>
+                    <li><a href="#"><img src="imagens/contato.png" alt="Contato" /> Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div id="submenu">
+            
+        </div>
+
+        <div id="conteudo">
+            
+        </div>
+
+        <div class="clear"></div>
+
+    </section>
 
 </body>
 </html>
