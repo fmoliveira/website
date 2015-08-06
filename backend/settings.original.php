a2124512c61a2626955823fd97499890bc0929e2
Filipe Oliveira <contato@fmoliveira.com.br>
2015-08-05 21:02:50 -0300

    Removing sensitive data.

---
+++ backend/settings.original.php
@@ -0,0 +1,15 @@
+<?php
+
+/* Tumblr integration. */
+define('TUMBLR_ACCOUNT_NAME', '');
+define('TUMBLR_CONSUMER_KEY', '');
+define('TUMBLR_CONSUMER_SECRET', '');
+
+/* Twitter integration. */
+define('TWITTER_ACCOUNT_NAME', '');
+define('TWITTER_CONSUMER_KEY', '');
+define('TWITTER_CONSUMER_SECRET', '');
+define('TWITTER_ACCESS_TOKEN', '');
+define('TWITTER_ACCESS_TOKEN_SECRET', '');
+
+?>
