8ca04ddb10ae40ab07755f070803c56513e1d6ca
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-25 20:57:25 -0300

    Added deploy script.

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Clean up old stuff
+DEPLOY_FOLDER=.deploy
+rm -rf $DEPLOY_FOLDER
+
+# Clone repository and erase latest build
+git clone -b master --single-branch git@github.com:fmoliveira/fmoliveira.github.io.git $DEPLOY_FOLDER
+git -C $DEPLOY_FOLDER rm -rf .
+
+# Execute build
+npm run build
+
+# Copy new build
+cp -r src/* $DEPLOY_FOLDER/.
+
+# Remove development files
+rm -rf $DEPLOY_FOLDER/styles/*.styl
+
+# Commit and push
+git -C $DEPLOY_FOLDER add --all .
+git -C $DEPLOY_FOLDER commit -m "Website deployment."
+git -C $DEPLOY_FOLDER push
+
+# Tear down clean up
+rm -rf $DEPLOY_FOLDER
