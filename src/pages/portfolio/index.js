229b4aaa79fb5dd9793decdd16eb3fc07380955a
Filipe Oliveira <contato@fmoliveira.com.br>
2018-04-30 23:05:43 -0300

    Added screenshots of NavalhApp.

---
+++ src/pages/portfolio/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { Container, Heading } from '../../components/styles'
+
+export const Portfolio = () => (
+  <Container>
+    <Heading>Portfolio</Heading>
+    <p>Coming soon, please come back in a few days... :)</p>
+  </Container>
+)
