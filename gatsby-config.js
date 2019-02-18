cbf50d5ade89858d22bd4b0419b999b438d06f35
Filipe Oliveira <contato@fmoliveira.com.br>
2019-02-18 10:23:02 -0300

    Configure manifest plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,8 +59,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Filipe Oliveira`,
+        short_name: `fmoliveira`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
