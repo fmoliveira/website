f2b5ed4236d977ce55ad91cdfbbba513f78f855c
Filipe Oliveira <contato@fmoliveira.com.br>
2019-02-18 10:21:36 -0300

    Setup analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-42379864-1`
       }
     },
     `gatsby-plugin-feed`,
