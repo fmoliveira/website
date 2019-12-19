2deee1292e4673e1e5b4b4780c93a47b686f08c8
Filipe Oliveira <contato@fmoliveira.com.br>
2019-12-19 18:37:42 -0300

    Use same header for all pages.

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,51 +7,7 @@ import Footer from "./footer";
 class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props;
-    const rootPath = `${__PATH_PREFIX__}/`;
-    let header;
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1),
-            marginBottom: rhythm(1.5),
-            marginTop: 0
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      );
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      );
-    }
     return (
       <div
         style={{
@@ -61,7 +17,26 @@ class Layout extends React.Component {
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
         }}
       >
-        <header>{header}</header>
+        <header>
+          <h1
+            style={{
+              ...scale(1),
+              marginBottom: rhythm(1.5),
+              marginTop: 0
+            }}
+          >
+            <Link
+              style={{
+                boxShadow: `none`,
+                textDecoration: `none`,
+                color: `inherit`
+              }}
+              to={`/`}
+            >
+              {title}
+            </Link>
+          </h1>
+        </header>
         <main>{children}</main>
         <Footer />
       </div>
