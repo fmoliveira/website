804cc37a4a74aa3117266f306895aade1e7b9844
Filipe Oliveira <contato@fmoliveira.com.br>
2020-04-21 16:41:52 -0300

    Move site title query to header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,32 @@
 import PropTypes from "prop-types"
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Box from "./Box"
 import Link from "./Link"
 import Title from "./Title"
 
-const Header = ({ siteTitle }) => (
-  <Box px={[4, 6]} py={[4, 5]} bg="headerBackground">
-    <Box m="0 auto" maxWidth={800}>
-      <Title as="h1">
-        <Link to="/">{siteTitle}</Link>
-      </Title>
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <Box px={[4, 6]} py={[4, 5]} bg="headerBackground">
+      <Box m="0 auto" maxWidth={800}>
+        <Title as="h1">
+          <Link to="/">{data.site.siteMetadata.title}</Link>
+        </Title>
+      </Box>
     </Box>
-  </Box>
-)
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,5 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 import { Helmet } from "react-helmet"
 import { Normalize } from "styled-normalize"
 import { ThemeProvider, createGlobalStyle } from "styled-components"
@@ -36,38 +28,26 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Normalize />
-      <GlobalStyle />
-      <Helmet>
-        <link
-          href="https://fonts.googleapis.com/css?family=Kameron&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,700&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <Box m="0 auto" p={[4, 6]} maxWidth={800}>
-        {children}
-      </Box>
-    </ThemeProvider>
-  )
-}
+const Layout = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <Normalize />
+    <GlobalStyle />
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css?family=Kameron&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+    <Header />
+    <Box m="0 auto" p={[4, 6]} maxWidth={800}>
+      {children}
+    </Box>
+  </ThemeProvider>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
