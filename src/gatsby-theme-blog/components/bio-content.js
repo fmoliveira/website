83c7d260603b1639091f4d91fb943d375669fabc
Filipe Oliveira <contato@fmoliveira.com.br>
2020-04-22 05:41:14 -0300

    Remove redundant link.

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -3,8 +3,8 @@ import { Styled } from "theme-ui"
 
 export default () => (
   <Fragment>
-    Hey! I'm <Styled.a href="https://fmoliveira.dev">Filipe Oliveira</Styled.a>,
-    a Frontend Engineer from Brazil! I write about my learning process and
-    experiences with React, TypeScript, Design Systems and Accessibility.
+    Hey! I'm <Styled.a>Filipe Oliveira</Styled.a>, a Frontend Engineer from
+    Brazil! I write about my learning process and experiences with React,
+    TypeScript, Design Systems and Accessibility.
   </Fragment>
 )
