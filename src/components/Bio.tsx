b72502d80cdc8f9bd6dcc1c64f6a955a0818fe7d
Filipe Oliveira <contato@fmoliveira.com.br>
2020-05-17 23:44:15 -0300

    Enlarge bio width.

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 
 const Bio: FC = () => (
-  <div className="max-w-md mb-8">
+  <div className="max-w-lg mb-8">
     <h1>Hello, I'm Filipe!</h1>
     <p>
       I'm a Frontend Engineer from Brazil. I write about my learning process and
