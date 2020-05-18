1b2c8dfa460a314bfae297041883bb03dda2e547
Filipe Oliveira <contato@fmoliveira.com.br>
2020-05-17 22:03:38 -0300

    Add post date.

--- src/templates/Post.tsx
+++ src/templates/Post.tsx
@@ -17,7 +17,8 @@ interface Props {
 
 const Post: FC<Props> = ({ data }) => (
   <Layout>
-    <h1>{data.markdownRemark.frontmatter.title}</h1>
+    <h1 className="mb-0">{data.markdownRemark.frontmatter.title}</h1>
+    <p className="mb-8 text-sm">{data.markdownRemark.frontmatter.date}</p>
     <div
       className="markdown"
       dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
