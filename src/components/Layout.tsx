b8f5d3c7f24c5c7c75d6bd7cd5e17738812f9ad2
Filipe Oliveira <contato@fmoliveira.com.br>
2020-05-18 00:01:22 -0300

    Add twitter link.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,11 +7,21 @@ interface LinkProps {
   to: string;
 }
 
-const NavLink: FC<LinkProps> = ({ children, to }) => (
-  <li className="mr-6">
-    <a href={to}>{children}</a>
-  </li>
-);
+const NavLink: FC<LinkProps> = ({ children, to }) => {
+  const external = to.startsWith("https://");
+
+  return (
+    <li className="mr-6">
+      <a
+        href={to}
+        target={external && "_blank"}
+        rel={external && "noopener noreferrer"}
+      >
+        {children}
+      </a>
+    </li>
+  );
+};
 
 const Layout: FC = ({ children }) => (
   <div className="container mx-auto px-8 py-4">
@@ -26,6 +36,7 @@ const Layout: FC = ({ children }) => (
         <NavLink to="/garden">Garden</NavLink>
         {/* <NavLink to="/lessons">Lessons</NavLink> */}
         {/* <NavLink to="/about">Contact</NavLink> */}
+        <NavLink to="https://twitter.com/tfmoliveira">Twitter</NavLink>
       </ul>
     </div>
     <div>{children}</div>
