7f461cbe02c6218bcc0bb8ed9aa5c0517a795a81
Filipe Oliveira <hi@fmoliveira.dev>
2021-04-02 20:32:06 -0300

    Install google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      sans:
+        'Montserrat, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      serif:
+        'Merriweather, ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+    },
   },
   variants: {
     extend: {},
