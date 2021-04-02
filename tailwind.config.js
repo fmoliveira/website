382aa35f4ab740910fe59848cd02b512191515f6
Filipe Oliveira <hi@fmoliveira.dev>
2021-04-02 17:44:01 -0300

    Setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/_includes/**/*.njk", "./src/**/*.md"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
