78f3663416f60ee9810dbf1c45b48786bd878c8d
Filipe Oliveira <hi@fmoliveira.dev>
2022-08-27 01:53:27 -0400

    Update meta tags

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -4,6 +4,10 @@ export default function PageHead() {
 	return (
 		<Head>
 			<title>Filipe Oliveira</title>
+			<meta
+				name="description"
+				content="Filipe Oliveira is a Software Engineer working with React and TypeScript"
+			/>
 		</Head>
 	)
 }
