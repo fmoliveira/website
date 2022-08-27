fdbd2386847c3cd4591a102543e55a88dccc112c
Filipe Oliveira <hi@fmoliveira.dev>
2022-08-26 23:43:17 -0400

    Setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./pages/**/*.{ts,tsx,js,jsx}",
+		"./components/**/*.{ts,tsx,js,jsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
